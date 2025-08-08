c0ec1b60aaf6a6a683bf6cfad49d90aead53f729
JaiMishraa <jai.mishra@goroots.us>
2025-08-08 11:07:42 +0530

    backend complete

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv, { parse } from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
+import transactionsRoute from './routes/transactionsRoute.js';
+import { initDB } from './config/db.js';
+
+const app= express();
+app.use(rateLimiter);
+app.use(express.json());
+
+const PORT = process.env.PORT || 5001;
+
+app.use('/api/transactions', transactionsRoute);
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
