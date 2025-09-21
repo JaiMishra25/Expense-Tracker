a719272904b18c069a943de512c79b8e16e2c28f
JaiMishraa <jai.mishra@goroots.us>
2025-09-21 18:50:56 +0530

    fixed some UI

--- mobile/hooks/useTransactions.js
+++ mobile/hooks/useTransactions.js
@@ -1,7 +1,7 @@
 import { useCallback, useState } from "react";
 import { Alert } from "react-native"; 
 
-const API_URL = "https://expense-tracker-q4jr.onrender.com/api";
+const API_URL = "http://localhost:5000/api";
 
 export const useTransactions = (userId) => {
     const [transactions, setTransactions] = useState([]);
